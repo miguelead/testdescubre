a5b91d3bcc7dfd4473572b8d505a29f8c86c4771
Momentum Lab 2 <momentumlab2@momentum-lab-2.local>
2017-11-09 17:01:50 -0400

    nov 9 5.01

---
+++ testdescubre/CustomTabBarController.swift
@@ -0,0 +1,65 @@
+//
+//  CustomTabBarController.swift
+//  testdescubre
+//
+//  Created by Momentum Lab 2 on 11/9/17.
+//  Copyright © 2017 Miguel Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = UIColor.hexStringToUIColor(hex: "11A791")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension UIColor{
+    
+    /**
+     Change hex code to UIColor
+     - returns: UIColor
+     - parameter hex: hex string code
+     */
+    static func hexStringToUIColor (hex: String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
